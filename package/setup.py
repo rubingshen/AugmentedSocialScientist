bc2c9a2ff64540586700559c5522d994d28b3c60
Hendrik Erz <hendrik@zettlr.com>
2023-04-26 14:37:36 +0200

    fix: Enable setup on macOS
    Allows this package to be installed on macOS

--- package/setup.py
+++ package/setup.py
@@ -8,7 +8,10 @@ environment = [
     'scipy',
     'nltk',
     'keras==2.8',
-    'tensorflow==2.8',
+    'tensorflow==2.8;sys_platform!="darwin"',
+    'tensorflow-macos==2.8;sys_platform=="darwin"',
+    # cf. https://stackoverflow.com/questions/72441758/typeerror-descriptors-cannot-not-be-created-directly
+    'protobuf==3.20.*',
     'torch',
     'transformers',
     'sentencepiece',
