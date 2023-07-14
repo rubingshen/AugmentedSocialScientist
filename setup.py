7b3df1e7f1948c6fae8d3340a65c559f2ccd2424
Rubing Shen <67069433+rubingshen@users.noreply.github.com>
2023-07-14 23:43:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ environment = [
     'sentencepiece',
 ]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(name='AugmentedSocialScientist',
       author='Rubing Shen',
       license='MIT',
@@ -21,7 +24,9 @@ setup(name='AugmentedSocialScientist',
       url='https://github.com/rubingshen/AugmentedSocialScientist',
       download_url='https://github.com/rubingshen/AugmentedSocialScientist/archive/refs/tags/v1.0.0.tar.gz',
       version='1.0.0',
-      description='a simple package to train bert-like model for text classification',
+      description='A Simple Package to Train Bert-Like Model for Text Classification',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       packages=['AugmentedSocialScientist'],
       zip_safe=False,
       install_requires=environment)
